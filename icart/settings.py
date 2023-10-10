1e88b37d192c4993377182f7cb86105c6c6d2e87
Subinkvs <122232846+Subinkvs@users.noreply.github.com>
2023-10-10 23:51:42 +0530

    Update settings.py

--- icart/settings.py
+++ icart/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'subinsundar9646k@gmail.com'
-EMAIL_HOST_PASSWORD = 'kdkn zolw btbc vkhh'
+EMAIL_HOST_PASSWORD = 
